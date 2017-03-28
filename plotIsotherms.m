5b20a631fa51db3eba295fd605b87d9f338c69d0
beslaw <beslaw@uchicago.edu>
2017-03-28 14:51:21 -0500

    Now with titles!
    Isotherms can now be plotted with titles pulled directly from the data
    file. The way this is written, it can't differentiate between first and
    second trials for titles and also can't make unique titles for more than
    9 isotherms at once

--- plotIsotherms.m
+++ plotIsotherms.m
@@ -1,8 +1,12 @@
 %make sure to navigate to the folder where the files to plot are first, the
 %program doesn't do that!
 
+%also, as written, this script cannot match titles to more than 9 isotherms
+%at once, so keep that in mind
+
 temp=dir;
 zzznames=cell((length(temp)-2),1);
+counter=1;  %for getting the names to match plots
 for n=3:length(temp)
    tempname=temp(n).name; 
    tempnum=length(tempname)-5;  %pull index of date from filename
@@ -11,17 +15,20 @@ for n=3:length(temp)
    dupeName=tempname(isDupe:length(tempname)); %store date with possibility of duplicate
    if double(dupeName(1))>57 %57 is the ascii code for 9
        [C,~]=importdata(tempname);
-       eval(['A' datename '= C.textdata(4:length(C.textdata),2);']);
-       eval(['P' datename '= C.textdata(4:length(C.textdata),3);']);
+       eval(['A' datename '_' num2str(counter) '= str2double(C.textdata(4:length(C.textdata),2));']);
+       eval(['P' datename '_' num2str(counter) '= str2double(C.textdata(4:length(C.textdata),3));']);
+       counter=counter+1;
    else
        [C,~]=importdata(tempname);
-       eval(['A_' dupeName(1) '_' datename '= C.textdata(4:length(C.textdata),2);']);
-       eval(['P_' dupeName(1) '_' datename '= C.textdata(4:length(C.textdata),3);']);
+       eval(['A_' dupeName(1) '_' datename '_' num2str(counter) '= str2double(C.textdata(4:length(C.textdata),2));']);
+       eval(['P_' dupeName(1) '_' datename '_' num2str(counter) '= str2double(C.textdata(4:length(C.textdata),3));']);
+       counter=counter+1;
    end
-   zzznames(n-2)=strcat(strtrim(C.textdata(2,1)),{' '},strtrim(C.textdata(2,7)));
+   zzznames(n-2)=strcat(strtrim(C.textdata(2,1)),{' '},strtrim(C.textdata(2,7)),{' '},datename);
 end
+%matching key system to facilitate sorting?
 
-clear tempname tempnum datename C n temp isDupe dupeName
+clear tempname tempnum datename C n temp isDupe dupeName counter
 
 varNames=who;
 zzznumVars=(length(varNames)-1);
@@ -57,12 +64,19 @@ clear areaCounter areaexp n pressureCounter str
 %containing the area info and the other the pressure, will match up. also
 %assuming that the loaded .mat file is non-emtpy
 
+%error with plot is that the data type is cell and not num
 for n=1:(zzznumVars/2)
    figure;
-   plot(eval(char(areas{n})),eval(char(pressures{n})));
-   title(areas{n});
+   xname=char(areas{n});
+   yname=char(pressures{n});
+   temp=length(xname);  %pull the index of the character representing the global index
+   indexy=xname(temp);  %pull value of the global index
+   xvalues=eval(xname);
+   yvalues=eval(yname);
+   plot(xvalues,yvalues);
+   title(zzznames{str2double(indexy)});
    xlabel('area ($\rm{\AA}^2$/molecule)','interpreter','latex');
    ylabel('surface pressure (mN/m)','interpreter','latex');
 end
 
-clear varNames n zzznumVars
+clear varNames n zzznumVars xvalues yvalues temp xname yname zzznames indexy areas pressures
